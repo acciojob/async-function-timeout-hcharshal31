263e0bc79a2253d0b12380dc53e5417e545533c9
Harshal Chavan <91681985+hcharshal31@users.noreply.github.com>
2025-02-16 01:55:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+let output = document.querySelector("#output");
+let btn = document.querySelector("#btn");
+
+async function delayMessage() {
+  let text = document.querySelector("#text").value;
+  let num = parseInt(document.querySelector("#delay").value, 10);
+
+  if(isNaN(num) || num < 0){
+    output.textContent = "Please enter the valid delay time.";
+    return;
+  }
+	
+  await new Promise((resolve)=>{
+    setTimeout(resolve, num);
+  })
+
+  output.textContent = text;
+}
+
+
+btn.addEventListener("click", ()=>{
+  delayMessage();
+})
